e55e8e7246e5df554ec973eec0727a8fb05f483c
olagraczyk <105671090+olagraczyk@users.noreply.github.com>
2023-05-19 12:46:10 +0200

    Update app.ts
    updated the code with drawing the challanges functionality

--- coremotivation/src/app.ts
+++ coremotivation/src/app.ts
@@ -1,19 +1,29 @@
 "use strict";
-
 //open and close modals
 function openModal(): void {
+  const challenges: string[] = [
+    "10 podciągnięć na gumie",
+    "2 podciągnięcia na drążku",
+    "3 razy wiszenie na drążku po minutę",
+    "3 razy po 20 brazylijskich przysiadów",
+    "2 razy deska po minutę",
+  ];
+
+  function losujChallenge(): string {
+    const randomIndex = Math.floor(Math.random() * challenges.length);
+    const selectedChallenge = challenges[randomIndex];
+    return selectedChallenge;
+  }
+
+  const selectedChallenge = losujChallenge();
+  const challengeTextElement = document.getElementById("challengeText");
+  if (challengeTextElement) {
+    challengeTextElement.textContent = selectedChallenge;
+  }
+
   document.getElementById("modal").style.display = "block";
+
+  function closeModal() {
+    document.getElementById("modal").style.display = "none";
+  }
 }
-
-function closeModal() {
-  document.getElementById("modal").style.display = "none";
-}
-
-//get a random challenge
-const challenges: string[] = [
-  "10 podciągnięć na gumie",
-  "2 podciągnięcia na drążku",
-  "3 razy wiszenie na drążku po minutę",
-];
-
-function losujChallenge() {}
